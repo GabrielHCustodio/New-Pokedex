a4e1e8247bee5651490a5941bce48b7543912cf0
GabrielHCustodio <93563385+GabrielHCustodio@users.noreply.github.com>
2022-03-25 22:07:34 -0300

    Created search button by the name of pokemon, function that picks up the name of the informed pokemon and improves the layout of the application.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,30 @@
 function pegarPokemons() {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=40')
     .then(response => response.json())
     .then(allPokemons => {
         var pokemons = []
 
         allPokemons.results.map((val) => {
-            console.log(val.name)
+            const name = val.name
+
+            pokemons.push(name)
         })
+
+        console.log(pokemons)
     })
 }
 
 pegarPokemons()
 
+const buttonSearch = document.querySelector('#buttonSearch')
+
+buttonSearch.addEventListener('click' , () => {
+    const nameSearch = document.querySelector('#nameSearch')
+
+    function research() {
+        const search = nameSearch.value
+        console.log(search)
+    }
+
+    research()
+})
